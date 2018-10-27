1a545e4508a9b561fe06623dd3230a47c41d5263
Ryan van den Bogaard <bogaardryan@hotmail.com>
2018-10-27 20:55:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,15 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+    if (message.content === '/help' || message.content === '/h') {
+    	message.channel.send('```C' + 
+                             '[/username] ' + 'check your current ingame username' + ' \n' +
+                             '[/username <new username>] ' + 'change your ingame username \'warning this is not immediately effective, please contact a mod when you do in #help-channel.\'' + ' \n' +
+                             '```');
+  	}
+});
+
+client.on('message', message => {
     if (message.content === '/username') {
     	message.reply(message.author.username);
   	}
