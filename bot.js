9368ca73272299a9b3a3abeb3d58735d4d1f81b2
Ryan van den Bogaard <bogaardryan@hotmail.com>
2018-10-30 20:29:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ debug_pixelmon : 506892880246341649
 //connected
 client.on('ready', () => {
 	console.log('向こうへ入るに!');
-	client.channels.find(self.debug_pixelmon).send('向こうへ入るに!');
+	client.channels.find("id", self.debug_pixelmon).send('向こうへ入るに!');
 });
 
 function dbCMD(sqlData){
